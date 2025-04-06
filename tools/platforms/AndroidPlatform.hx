af1553c40bd103de5eb7b91a88cd4ba69294df83
HomuHomu833 <egzozu.be1.bas@gmail.com>
2025-04-06 14:04:53 +0300

    add missing `requestLegacyExternalStorage`

--- tools/platforms/AndroidPlatform.hx
+++ tools/platforms/AndroidPlatform.hx
@@ -490,6 +490,7 @@ class AndroidPlatform extends PlatformTarget
 			"android:theme": "@style/LimeAppMainTheme" + (project.window.fullscreen ? "Fullscreen" : ""),
 			"android:hardwareAccelerated": "true",
 			"android:allowNativeHeapPointerTagging": context.ANDROID_TARGET_SDK_VERSION >= 30 ? "false" : null,
+			"android:requestLegacyExternalStorage": context.ANDROID_TARGET_SDK_VERSION >= 29 ? "true" : null,
 			"android:largeHeap": "true"
 		});
 		context.ANDROID_ACTIVITY = project.config.getKeyValueArray("android.activity", {
